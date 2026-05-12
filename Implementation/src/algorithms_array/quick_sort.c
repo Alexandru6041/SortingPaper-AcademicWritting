ffd97328ae063b1efc1a5847233815071b9f9212
Chelariu Alexandru <alexchelariu834@gmail.com>
2026-05-12 12:31:18 +0300

    Update

--- Implementation/src/algorithms_array/quick_sort.c
+++ Implementation/src/algorithms_array/quick_sort.c
@@ -11,9 +11,9 @@ static void quick_sort_recursive(int *arr, int low, int high) {
         int mid = low + (high - low) / 2;
         int pivot = arr[mid];
 
-        int lt = low;   // Zona elementelor < pivot
-        int i = low;    // Zona elementelor == pivot
-        int gt = high;  // Zona elementelor > pivot
+        int lt = low; 
+        int i = low;
+        int gt = high;
 
         while (i <= gt) {
             if (arr[i] < pivot) {
